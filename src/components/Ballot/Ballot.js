cf839f3f9aec1066df55344d92ccf2368652ec47
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-11-07 18:20:08 +0100

    feat: ballot component updated

--- src/components/Ballot/Ballot.js
+++ src/components/Ballot/Ballot.js
@@ -1,12 +1,34 @@
-const Ballot = () => {
-  return (
-    <div className="ballot">
-      <header className="ballot__header">
-        <h3></h3>
-      </header>
+import useBallots from "../../hooks/useBallots";
+import { ErrorAlert } from "../../shared/alert/Alert";
+import Card from "../../shared/card/Card";
+import Spinner from "../../shared/spinner/Spinner";
 
-      <div className="ballot__grid"></div>
-    </div>
+const Ballot = () => {
+  const { ballots, isLoading, error } = useBallots();
+  console.log(ballots);
+  return (
+    <>
+      {isLoading && <Spinner />}
+      {error && <ErrorAlert message={error} />}
+
+      <div className="ballot">
+        {ballots &&
+          ballots.map(({ id, title, items }) => (
+            <div key={id}>
+              <header className="ballot__header">
+                <h3>{title}</h3>
+              </header>
+
+              <div className="ballot__grid">
+                {items &&
+                  items.map((item, index) => (
+                    <Card ballot={item} key={item.id} />
+                  ))}
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
