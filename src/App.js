674a60e26907eb61b399c217bcdb0266854af08a
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-11-07 15:08:39 +0100

    feat: add header component and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Ballot from "./components/ballot/Ballot";
 import Container from "./components/container/Container";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <Container>
+      <Header />
       <Ballot />
     </Container>
   );
---
+++ src/components/header/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h2>Awards {new Date().getFullYear()}</h2>
+    </header>
+  );
+};
+
+export default Header;
