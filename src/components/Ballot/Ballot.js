0e1c25a1a0d8af82b032996e1f1535828ea8610f
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-11-07 17:19:46 +0100

    feat: add ballot skeleton

--- src/components/Ballot/Ballot.js
+++ src/components/Ballot/Ballot.js
@@ -1,11 +1,13 @@
-import React from 'react';
-
 const Ballot = () => {
   return (
-    <div className='ballot'>
-      'Your Code Goes Here'
+    <div className="ballot">
+      <header className="ballot__header">
+        <h3></h3>
+      </header>
+
+      <div className="ballot__grid"></div>
     </div>
-  )
-}
+  );
+};
 
 export default Ballot;
